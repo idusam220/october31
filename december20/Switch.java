5c5bdff5b74152327400b6d7622eb46b54ae0cd4
Samuel Iduherubuntu <idusam220@gmail.com>
2023-12-20 23:31:28 -0700

    Switch example

---
+++ december20/Switch.java
@@ -0,0 +1,25 @@
+public class Switch
+{
+	public static void main(String[]args)
+	{
+	String day = "Mango";
+	  switch(day){
+
+	    case "Tuesday":System.out.println("It's Tuesday");
+	  	break;
+	  	case "Wednesday":System.out.println("It's Wednesday");
+	  	break;
+	  	case "Thursday":System.out.println("It's Thursday");
+	  	break;
+	  	case "Friday":System.out.println("It's Friday");
+	  	break;
+	  	case "Saturday":System.out.println("It's Saturday");
+	  	break;
+	    case "Sunday":System.out.println("It's Sunday");
+	  	break;
+	  case "Monday":System.out.println("It's Monday");
+	  	break;
+	  default:System.out.println("Not in days of the week");
+	  }
+	}
+}
