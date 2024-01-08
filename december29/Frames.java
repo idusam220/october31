986ef345c4cf7af9991fa9d7f218f8585347e436
Samuel Iduherubuntu <idusam220@gmail.com>
2024-01-08 12:57:24 -0700

    Frames in java

---
+++ december29/Frames.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class Frames
+{
+	public static void main(String[]args)
+	{
+	   JFrame frame = new JFrame();
+	   frame.setVisible(true);
+	   frame.setSize(500,500);
+	}
+}
